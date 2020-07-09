4981b1f93a73263200334d6f65372ad7ca2136f5
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 00:02:44 +0530

    UseCase7 - Employee Hours using funtions Update

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,30 +1,34 @@
 #!/bin/bash -x
 
+#constants
 wage_per_hour=15
 work_days_per_month=20
+
+#variables
 working_days=0
 total_work_hours=0
 
+#function to calculate workhours
+function empWorkHours(){
+   isfulltime=1
+   isparttime=2
+     case $1 in
+            $isfulltime)
+                work_hours=8 ;;
+            $isparttime)
+                work_hours=4 ;;
+            *)
+                work_hours=0 ;;
+     esac
+   echo $work_hours
+}
+
+#condition to check for total work hours or work days
 while [[ ($total_work_hours -le 100) || ($working_days -le $work_days_per_month) ]]
 do
-isfulltime=1
-isparttime=2
-empCheck=`echo $((RANDOM%3))`
-   case $empCheck in
-      $isfulltime)
-            work_hours=8
-            echo "Employee Present"
-            total_work_hours=$((total_work_hours+work_hours));;
-      $isparttime)
-            work_hours=4
-            echo "Employee works part time"
-            total_work_hours=$((total_work_hours+work_hours)) ;;
-      *)
-            work_hours=0
-            echo "Employee Absent"
-            total_work_hours=$((total_work_hours+work_hours)) ;;
-   esac
-((working_days++))
+      work_hours_per_day=$( empWorkHours $((RANDOM%3)) )
+      total_work_hours=$((total_work_hours+work_hours_per_day))
+      ((working_days++))
 done
 
 echo "Employee Wage: "$((wage_per_hour*total_work_hours))
