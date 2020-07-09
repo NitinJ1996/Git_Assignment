44ae5cf1bb48623a1559591d3ec11fd0aee84c8e
NitinJ1996 <nitin1996j@gmail.com>
2020-07-09 23:55:08 +0530

    UseCase  5- Employee per month Wage calulation Update

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,19 +1,30 @@
 #!/bin/bash -x
 
+wage_per_hour=15
+work_days_per_month=20
+working_days=0
+total_work_hours=0
+
+while [ $working_days -le $work_days_per_month ]
+do
 isfulltime=1
 isparttime=2
 empCheck=`echo $((RANDOM%3))`
-wage_per_hour=15
-case $empCheck in
-        $isfulltime)
-                work_hours=8
-                echo "Employee Present";;
-        $isparttime)
-                work_hours=4
-                echo "Employee works part time" ;;
-        *)
-                work_hours=0
-                echo "Employee Absent" ;;
-esac
+   case $empCheck in
+      $isfulltime)
+            work_hours=8
+            echo "Employee Present"
+            total_work_hours=$((total_work_hours+work_hours));;
+      $isparttime)
+            work_hours=4
+            echo "Employee works part time"
+            total_work_hours=$((total_work_hours+work_hours)) ;;
+      *)
+            work_hours=0
+            echo "Employee Absent"
+            total_work_hours=$((total_work_hours+work_hours)) ;;
+   esac
+((working_days++))
+done
 
-echo "Employee Wage: "$((wage_per_hour*work_hours ))
+echo "Employee Wage: "$((wage_per_hour*total_work_hours))
