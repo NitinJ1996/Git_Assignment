21f262d24690e132ac0773167f74f53e87bf626a
NitinJ1996 <nitin1996j@gmail.com>
2020-07-09 23:43:01 +0530

    UseCase 2 - Employee Wage Calculation updated

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%2))
+wage_per_hour=20
+empCheck=$((RANDOM%3))
 if [ $empCheck -eq 1 ]
 then
-		echo "Employee Present"
+      echo "Employee Works full day"
+      workHours=8
 else
-		echo "Employee Absent"
+      echo "Employee Absent"
+      workHours=0
 fi
+
+echo "Employee Wage: "$((wage_per_hour*workHours))
