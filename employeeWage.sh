a30704520acccd253e40a8b3dcf85078991cf13c
NitinJ1996 <nitin1996j@gmail.com>
2020-07-09 23:48:17 +0530

    UseCase 3- Employee Part Time Attendance check Updated

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,10 +5,14 @@ empCheck=$((RANDOM%3))
 if [ $empCheck -eq 1 ]
 then
       echo "Employee Works full day"
-      workHours=8
+      work_hours=8
+elif [ $empCheck -eq 2 ]
+then
+      echo "Employee Works Part time"
+      work_hours=4
 else
       echo "Employee Absent"
-      workHours=0
+      work_hours=0
 fi
 
-echo "Employee Wage: "$((wage_per_hour*workHours))
+echo "Employee Wage: "$((wage_per_hour*work_hours))
