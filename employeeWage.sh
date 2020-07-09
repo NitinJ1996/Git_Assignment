60820c296aa694c3311f1635839ccdd082a7fbf4
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 00:09:11 +0530

    UseCase 9 - Employee Wages stored in Dictionary Update

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 
-#function to calculate work hours
 function empWorkHours(){
 isfulltime=1
 isparttime=2
 case $1 in
-      $isfulltime) work_hours=8 ;;
-      $isparttime) work_hours=4 ;;
+        $isfulltime) work_hours=8 ;;
+        $isparttime) work_hours=4 ;;
       *)       work_hours=0 ;;
 esac
    echo $work_hours
@@ -19,20 +18,23 @@ work_days_per_month=20
 #variables
 total_work_hours=0
 working_day=0
-counter=0
 
 function empWage(){
    echo $(($wage_per_hour*$1))
 }
 
+declare -A wage_dict
 while [[ ($total_work_hours -le 100) && ($working_day -le $work_days_per_month) ]]
 do
    ((working_day++))
    work_hours_per_day=$( empWorkHours $((RANDOM%3)) )
-   wages_array[((counter++))]=$( empWage $work_hours_per_day )
+   wage_dict["Day $working_day"]=$( empWage $work_hours_per_day )
    total_work_hours=$((total_work_hours+work_hours_per_day))
 done
 
 total_wage=$( empWage $total_work_hours )
 echo "Total Wage: "$total_wage
-echo "Per day wages as array: "${wages_array[@]}
+for num in "${!wage_dict[@]}"
+do
+   echo $num" Wage: "${wage_dict[$num]}
+done
