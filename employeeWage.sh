772dcf9fd5139ab41c1e1cb892e97278c02ee054
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 00:06:02 +0530

    UseCase 8 - Employee Wages stored in Arrays Update

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,34 +1,38 @@
 #!/bin/bash -x
 
+#function to calculate work hours
+function empWorkHours(){
+isfulltime=1
+isparttime=2
+case $1 in
+      $isfulltime) work_hours=8 ;;
+      $isparttime) work_hours=4 ;;
+      *)       work_hours=0 ;;
+esac
+   echo $work_hours
+}
+
 #constants
 wage_per_hour=15
 work_days_per_month=20
 
 #variables
-working_days=0
 total_work_hours=0
+working_day=0
+counter=0
 
-#function to calculate workhours
-function empWorkHours(){
-   isfulltime=1
-   isparttime=2
-     case $1 in
-            $isfulltime)
-                work_hours=8 ;;
-            $isparttime)
-                work_hours=4 ;;
-            *)
-                work_hours=0 ;;
-     esac
-   echo $work_hours
+function empWage(){
+   echo $(($wage_per_hour*$1))
 }
 
-#condition to check for total work hours or work days
-while [[ ($total_work_hours -le 100) || ($working_days -le $work_days_per_month) ]]
+while [[ ($total_work_hours -le 100) && ($working_day -le $work_days_per_month) ]]
 do
-      work_hours_per_day=$( empWorkHours $((RANDOM%3)) )
-      total_work_hours=$((total_work_hours+work_hours_per_day))
-      ((working_days++))
+   ((working_day++))
+   work_hours_per_day=$( empWorkHours $((RANDOM%3)) )
+   wages_array[((counter++))]=$( empWage $work_hours_per_day )
+   total_work_hours=$((total_work_hours+work_hours_per_day))
 done
 
-echo "Employee Wage: "$((wage_per_hour*total_work_hours))
+total_wage=$( empWage $total_work_hours )
+echo "Total Wage: "$total_wage
+echo "Per day wages as array: "${wages_array[@]}
